097ef92f7a30d1ae30921a69408373f4b431e8c5
sosrtv353 <sosortveliashvili@gmail.com>
2024-10-14 23:19:55 +0400

    second commit

---
+++ level 02/homework/homework2.py
@@ -0,0 +1,33 @@
+print("basketball is a fun sport!")
+print("in one team there are 5 players")
+print("to win your team must have higher points than other team")
+print("there are different positions in basketball: center, shooting guard, point guard, small forward and power forward")
+print("basketball takes a lot of time to master")
+print("there is offence and defense just like some sports")
+print("there are a lot of statistics in basketball:assists, points,rebounds,steals,blocks, and etc")
+print("on defence you have to guard an oppoment and not let him score")
+print("on offence you have to make opportunities for the team to score or to do it yourself")
+print("a game starts with a jump ball")
+print("for the jump ball 2 centers must jump in the middle of the court and try to tip it when refree throws the ball in the air")
+print("there is also a three point line which is far from the basket. if you score from there you get three points")
+print("but if you cant shoot far you can try midrange shooting")
+print("you can also do a layup or a dunk, but dunking takes a lot of training and strengthening")
+print("in nba for the game to end it takes 48 minutes")
+print("there is also other organizations like fiba which is played in europe with different rules")
+print("basketball is second most popular sport standing behind football")
+print("the greatest basketball player is considered michael jordan which made this sport so popular")
+print("there are a lot of great players like: lebrom james, tim duncan,larry bird and stephen curry")
+print("overall basketball is a very fun sport which takes skills!")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 02/homework/homework3.py
@@ -0,0 +1,21 @@
+healthy= "go to gym,eat healthy and get enough sleep "
+avoid="junk food, being lazy, smoking and drinking alcohol"
+mind="study,read books and take advice from succesful person"
+excresise="pushups situps and  squats "
+suggest="studying in GOA academy"
+
+print("its important to be healthy! ")
+print("being healthy makes you live longer without problems and makes you happy")
+print("to be healthy you first should"+ healthy,"and avoid"+avoid )
+print("apart from having a healthy body you should have a healthy mind ")
+print("to get a healthy mind you will have to"+ mind)
+print("there are a lot of excressises.the most famous ones are"+excresise)
+print("doing this excresises will make you stronger! but be sure to be consistent")
+print("i recommend"+ suggest,"this way you will do both!")
+
+
+
+
+
+# ცვლადი პროგრამირებაში არის "ყუთი" სადაც ინახება ტექსტური ინფორმაცია.
+#ცვლადი კოდინგში გვეხმარება უფრო მოკლედ წერაში.
