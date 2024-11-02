ae7421f0db7cf297859646206504a33530c33da6
sosrtv353 <sosortveliashvili@gmail.com>
2024-11-02 19:14:53 +0400

    second commit

---
+++ level 08/classwork/classwork.py
@@ -0,0 +1,37 @@
+name=input("please enter your name:")
+print(name)
+print(type(name))
+surname=input("enter your surname for sign up:")
+print(surname)
+print(type(surname))
+age=input("enter your age:")
+print(age)
+print(type(age))
+num1=input("enter your desired number:")
+print(num1)
+print(type(num1))
+num2=("enter how many items you want to select:")
+print(num2)
+print(type(num2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 08/homework/homework.py
@@ -0,0 +1,24 @@
+num1=3
+num2=5
+num3=6
+num4=8
+num5=10
+print(num1-num2+(num5/num2*num4)**num1%num3)
+print((num3/num1)**num2+num5%num4)
+print(num4%num3*num5/num2**num1)
+print(num2*num5-(num1+num4)/num3)
+print((num5-num4)**num1%num3*num2)
+print(num2-num1*num3%num4**num5)
+print((num5%num4+num3-num2)/num1)
+print(((num1+num5-num4)/num2 )**num3)
+print(num2-num1*(num4-num5)+num3)
+print(num3%num2*num5-num4+num1)
+
+
+name=input("please type your name")
+surname=input("we also need your surname for the login")
+print("გამარჯობა"+ name,surname)
+
+
+#input არის ინფორმაცია რაც შედის პროგრამაში
+#ხოლო output არის ინფორმაცია რაც გამოდის პროგრამიდან
