a1e01a589081da1ac039fc25209f580f25b57040
sosrtv353 <sosortveliashvili@gmail.com>
2024-11-22 16:52:11 +0400

    third commit

---
+++ level 14/classwork/homework.py
@@ -0,0 +1,18 @@
+for r in range(20):
+    print("soso rtveliashvili")
+
+
+num2=input(int("enter a number"))
+num3=input(int("enter a number"))
+for a in range(10):
+    num2**num2
+    num3**num3
+
+
+num1,num6=5, 10
+
+
+
+number01=int(input("enter a number"))
+for w in range(0, number01):
+    print(range*2)
