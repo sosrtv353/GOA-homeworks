a88af5c52ed456a1fcc621eee22ab9f1a4f02aef
sosrtv353 <sosortveliashvili@gmail.com>
2024-11-28 17:43:39 +0400

    third commit

--- level 12/classwork/classwork.py
+++ level 12/classwork/classwork.py
@@ -1,1 +1,14 @@
-#no classwork
+print(True or False ) #True
+print(False or True) #True
+print(True or True) # true
+print(False or False) #false
+
+print(True and True) #true
+print(True and False ) #false
+print(False and True) #false
+print(False and False) #false 
+
+numb1=input(int("enter a number:"))
+numb2=input(int("enter a second number:"))
+print(numb1>30)
+print(numb2<40)
